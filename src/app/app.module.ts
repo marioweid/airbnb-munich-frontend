069c5dd3edc059008d413c5c132ec2528d4e72dc
S4ltyGo4t <mario.weidner@gmx.de>
2020-02-24 21:10:46 +0100

    added material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CsvTableComponent} from './csv-table/csv-table.component';
+import {MaterialDesignModule} from './material-design/material-design.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CsvTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
