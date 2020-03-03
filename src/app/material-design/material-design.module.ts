c0a2323738742c6855573db3eb1941c1cdf43201
S4ltyGo4t <mario.weidner@gmx.de>
2020-03-03 18:19:26 +0100

    added heroku deployment

--- src/app/material-design/material-design.module.ts
+++ src/app/material-design/material-design.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 const modules: any[] = [
   MatButtonModule,
-  MatMenuModule
+  MatMenuModule,
+  MatToolbarModule
 ];
 
 @NgModule({
